e1e603e85720c1536c4c8a1a0343249962deed25
sachin tedwa <tedwasachin123@gmail.com>
2023-02-28 15:14:04 +0530

    golang_variable

---
+++ variable.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+
+//
+
+func main(){
+	// unused variables always give an error
+
+	//1st method
+    var a int //declaration
+	a = 55    //initialization
+
+    //2nd method
+	var b int = 5
+
+    //3rd method
+	var c = 100
+
+	//4th method
+	d := 500 
+	
+	fmt.Println(a,b,c,d)
+}
