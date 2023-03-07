2b734c2c1f482f64d5a5edc0468526629e24bd09
sachin tedwa <tedwasachin123@gmail.com>
2023-03-07 12:01:44 +0530

    golang_if_condition

---
+++ 11If_Conditions.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+
+func main()  {
+	//Braces must open in the same line of the if/else ( Aghh 😕 )
+	//in Go's if-statements parentheses( ) around conditions are optional. but the braces { } are required even for oneliners.
+
+	value := 10
+	if value < 10 {
+	  println("Less Than 10")
+	} else if value > 10 {
+	  println("Greater Than 10")
+	} else {
+	  println("Equals 10")
+	}
+  
+	//if conditions with statment
+	//note that value is inscope of all if/else's 
+	if 	value := 10; value < 10 {
+	  println(value, "Less Than 10")
+	} else if value > 10{
+	  println(value, "Greater Than 10")
+	}else{
+	  println(value, "Equals 10")
+	}
+}
